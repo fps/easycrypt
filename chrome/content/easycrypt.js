39b1f9a52795a64619e21ebeec9ef56b4742daca
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2011-12-04 02:32:15 +0100

    one dialog instead of two :D

--- chrome/content/easycrypt.js
+++ chrome/content/easycrypt.js
@@ -291,26 +291,42 @@ var easycrypt = {
 		  panel.openPopup(document.getElementById("navigator-toolbox"), "after_start");
 		  panel.textthingy = document.popupNode;
 
-		  var b = document.getElementById("easycrypt-button");
+		  var b = document.getElementById("easycrypt-encrypt-button");
 		  b.removeEventListener("click", easycrypt.encrypt, false);
 		  b.addEventListener("click", easycrypt.encrypt, false);
 
-		  var b2 = document.getElementById("easycrypt-insert-button");
+		  var b3 = document.getElementById("easycrypt-decrypt-button");
+		  b3.removeEventListener("click", easycrypt.decrypt, false);
+		  b3.addEventListener("click", easycrypt.decrypt, false);
+
+		  var b2 = document.getElementById("easycrypt-encrypt-and-insert-button");
 		  b2.removeEventListener("click", easycrypt.encrypt_and_insert, false);
 		  b2.addEventListener("click", easycrypt.encrypt_and_insert, false);
 		  // var doc = aEvent.originalTarget;
 
+		  var focusedWindow = document.commandDispatcher.focusedWindow;
+		  var text = focusedWindow.getSelection().toString();
+		  document.getElementById("easycrypt-clear-textfield").value = text;
+
+		  easycrypt.encrypt();
+
 	 },
 
 	 decrypt_show: function(e) {
-		  var panel =  document.getElementById("easycryptDecryptpanel");
+		  var panel =  document.getElementById("easycryptEncryptpanel");
 		  panel.openPopup(document.getElementById("navigator-toolbox"), "after_start");
 		  panel.textthingy = document.popupNode;
 		  var doc = document.popupNode;
 		  var focusedWindow = document.commandDispatcher.focusedWindow;
 		  var text = focusedWindow.getSelection().toString();
+		  document.getElementById("easycrypt-crypt-textfield").value = text;
 		  // alert(text);
 
+		  easycrypt.decrypt();
+	 },
+
+	 decrypt: function(e) {
+		  var text = document.getElementById("easycrypt-crypt-textfield").value;
 		  var matches = text.match(/easycrypt\[\[\[.*?\]\]\]/g);
 		  var cyphers = matches;
 
@@ -320,22 +336,22 @@ var easycrypt = {
 				cyphers[i] = matches[i].substring(12, matches[i].length - 3);
 				text = text.replace("easycrypt[[[" + cyphers[i] + "]]]", Crypto.AES.decrypt(cyphers[i], prefManager.getCharPref("extensions.easycrypt.pw1")));
 		  }
-		  document.getElementById("easycrypt-cleartext-textfield").value = text;
+		  document.getElementById("easycrypt-clear-textfield").value = text;
 	 },
 
-	 encrypt: function(e) {
+	 encrypt: function() {
 		  var panel =  document.getElementById("easycryptEncryptpanel");
-		  var cleartext = document.getElementById("easycrypt-textfield").value;
+		  var cleartext = document.getElementById("easycrypt-clear-textfield").value;
 		  // alert("foo");
 		  // document.getElementById("easycrypt-cypher-textfield").value = "ugh";
 		  // TODO: encrypt :D
 		  var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-		  document.getElementById("easycrypt-cypher-textfield").value = "easycrypt[[[" + Crypto.AES.encrypt(cleartext, prefManager.getCharPref("extensions.easycrypt.pw1")) + "]]]";   
+		  document.getElementById("easycrypt-crypt-textfield").value = "easycrypt[[[" + Crypto.AES.encrypt(cleartext, prefManager.getCharPref("extensions.easycrypt.pw1")) + "]]]";   
 	 },
 
 	 encrypt_and_insert: function(e) {
 		  var panel =  document.getElementById("easycryptEncryptpanel");
-		  var cleartext = document.getElementById("easycrypt-textfield").value;
+		  var cleartext = document.getElementById("easycrypt-clear-textfield").value;
 
 		  var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 		  // TODO: encrypt :D
